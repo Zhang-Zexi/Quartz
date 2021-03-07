b16b3b57ec2d761260c0193c41a30cd15aea49c2
Zhang-Zexi <2445459509@qq.com>
2021-03-07 16:14:25 +0800

    test: test

--- src/main/java/com/zzx/math/demo/MathDemo.java
+++ src/main/java/com/zzx/math/demo/MathDemo.java
@@ -1,2 +1,16 @@
-package com.zzx.math.demo;public class MathDemo {
+package com.zzx.math.demo;
+
+import java.math.BigDecimal;
+
+public class MathDemo {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i / 100);
+        int l = (int) (i * 0.01);
+        System.out.println(l);
+        BigDecimal bigDecimal = BigDecimal.valueOf(i);
+        System.out.println(bigDecimal.multiply(BigDecimal.valueOf(0.01)));
+    }
+
 }
